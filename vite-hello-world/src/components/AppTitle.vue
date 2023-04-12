3a70f472da35ab82991a2b44c4cef3f47b6bc5d1
JorgeLuix <tec.jorge.29@gmai.com>
2023-04-12 16:34:54 +0200

    inserito primo componente

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ titolo }} </h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppTitle',
+        data() {
+            return {
+                titolo: 'La mia prima app con Vite!'
+            }
+            
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
